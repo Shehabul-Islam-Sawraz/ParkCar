c3a38bcd8275b54c27ac6c3fe5c80c6f5eb682b2
Shehabul-Islam-Sawraz <1805088@ugrad.cse.buet.ac.bd>
2023-02-18 18:41:25 +0600

    Zoom & fit the map on search done(errors not resolved)

--- ParkCar/components/Map.js
+++ ParkCar/components/Map.js
@@ -1,5 +1,5 @@
 import { Text, View } from 'react-native';
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import tw from 'tailwind-react-native-classnames';
 import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import { useSelector } from 'react-redux';
@@ -7,23 +7,42 @@ import { selectDestination, selectSource } from '../slices/navSlice';
 import MapViewDirections from 'react-native-maps-directions';
 import { GOOGLE_MAPS_API_KEY } from "@env";
 
+
 const Map = () => {
   const source = useSelector(selectSource);
   const destination = useSelector(selectDestination);
+  const mapRef = useRef(null); // Making a reference to map, so we can change its characteristics
+
+  useEffect(() => {
+      if(!source || !destination){ // If source or destination is not selected, then return
+        return;
+      }
+
+      // Zoom & fit to markers
+      // Here `source` & `destination` are the identifier we have initialized in this page below
+      mapRef.current
+          .fitToSuppliedMarkers(['source', 'destination'], {
+            animated: true,  
+            edgePadding: { top: 50, right: 50, bottom: 50, left: 50 }, // This edgePadding will make a padding to the map markers, so that they don't show up on the screen edge
+          });
+      
+  }, [source, destination])
 
   return (
     <MapView
+        ref = {mapRef}
         style = {tw`flex-1`}
         provider = {PROVIDER_GOOGLE}
         mapType = "standard"
         initialRegion={{
             latitude: source.location.lat,
             longitude: source.location.lng,
-            latitudeDelta: 0.005,
-            longitudeDelta: 0.005,
+            latitudeDelta: 0.0222,
+            longitudeDelta: 0.0121,
         }}
+        
     >
-      {/**?. says that source may be undefined, so its optional*/}
+
       {source && destination && (
         <MapViewDirections
           origin = {source.description}
@@ -33,6 +52,8 @@ const Map = () => {
           strokeColor = "black"
         />
       )}
+
+      {/**?. says that source may be undefined, so its optional*/}
       {source?.location && (
         <Marker
           coordinate = {{
@@ -42,6 +63,18 @@ const Map = () => {
           title = "Origin"
           description= {source.description}
           identifier = "source"
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+          coordinate = {{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+          }}
+          title = "Destination"
+          description= {destination.description}
+          identifier = "destination"
         />
       )}
     </MapView>
